dbf2cad4c5bb6e9745646890b834f6a992508e75
murali0101 <muralimv0101@gmail.com>
2022-06-03 17:19:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
-
+const restaurantController = require("./controllers/restaurant.controller");
 app.get("/", (req, res) => {
   try {
     res.status(200).send("welcome to NEARBY.com");
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
     res.status(400).send({ error: error.message });
   }
 });
-
+app.use("/geo", restaurantController);
 
 module.exports = app;
